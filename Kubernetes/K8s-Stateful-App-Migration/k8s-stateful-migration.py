8d0de83a1df305c2d463a54a0c166603bebe7349
EnricoValastro <79710717+EnricoValastro@users.noreply.github.com>
2023-05-29 17:34:25 +0200

    StatefulMigrationTime

--- Kubernetes/K8s-Stateful-App-Migration/k8s-stateful-migration.py
+++ Kubernetes/K8s-Stateful-App-Migration/k8s-stateful-migration.py
@@ -1,5 +1,7 @@
 from kubernetes import client, config
 import subprocess
+import datetime;
+import dateutil.tz
 
 config.load_kube_config()
 v1 = client.CoreV1Api()
@@ -23,6 +25,8 @@ user="cb0@"
 path= "/home/cb0/state"
 dpath= "/home/cb0"
 
+date_format = '%Y-%m-%d %H:%M:%S%z'
+
 def execute_command(command):
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     output, error = process.communicate()
@@ -39,6 +43,9 @@ def copy_file_between_nodes(source_node, destination_node, path):
 
 node_list = v1.list_node()
 
+# Migration start
+migration_start = datetime.datetime.now(dateutil.tz.tzlocal())
+
 # Get node list, find source and destination ip, swap labels.
 for n in node_list.items:
     if (('type', 'source') in n.metadata.labels.items()):
@@ -49,10 +56,41 @@ for n in node_list.items:
         patch_node_lab_res = v1.patch_node(n.metadata.name, source)
 
 
+# State migration begin
+state_migration_start = datetime.datetime.now(dateutil.tz.tzlocal())
+
 copy_file_between_nodes(source_node, destination_node, path)
+
+# State migration end
+state_migration_end = datetime.datetime.now(dateutil.tz.tzlocal())
 
 pod_list = v1.list_namespaced_pod('default', label_selector="app=td")
 for pod in pod_list.items:
     if("td-deployment" in pod.metadata.name):
+        # App migration start
         delete_pod_response = v1.delete_namespaced_pod(pod.metadata.name, 'default')
+        
+        # Downtime begin
+        downtime_begin = datetime.datetime.now(dateutil.tz.tzlocal())
 
+        res = v1.list_namespaced_pod('default', label_selector="app=td")
+        x = True
+        while(res.items[0].metadata.name == pod.metadata.name or x):
+            if(res.items[0].metadata.name != pod.metadata.name):
+                try:
+                    res.items[0].status.container_statuses[0].state.running.started_at
+                    x = False
+                    break
+                except:
+                    x = True
+            res = v1.list_namespaced_pod('default', label_selector="app=td")
+        downtime_end = datetime.datetime.now(dateutil.tz.tzlocal())
+        migration_end = datetime.datetime.now(dateutil.tz.tzlocal())
+
+tot_time = migration_end - migration_start
+data_migration_time = state_migration_end - state_migration_start
+downtime = downtime_end - downtime_begin
+
+print("Total time: "+str(tot_time))
+print("State migration: "+str(data_migration_time))
+print("Downtime: "+str(downtime))
